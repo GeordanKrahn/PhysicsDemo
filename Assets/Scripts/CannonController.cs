ac4ca1f34e029f4ee17693485566b7874f29e7a3
Geordan Krahn <geordankrahn@gmail.com>
2022-04-10 10:21:40 -0600

    added in some comments to explain the code.

--- Assets/Scripts/CannonController.cs
+++ Assets/Scripts/CannonController.cs
@@ -5,6 +5,21 @@ using System;
 
 public class CannonController : MonoBehaviour
 {
+    // The following 4 properties and fields
+    // control the interpolator.
+    public float maxRotationTime;
+    public int numInterpolations;
+    public float timeStep
+    {
+        get
+        {
+            return maxRotationTime / (float)numInterpolations;
+        }
+    }
+    
+    public float rotationTime = 0;
+
+    // the following 4 properties and fields keep track of the projectile positions.
     public List<Transform> ListOfTargets;
     public GameObject projectile;
     private Tuple<Vector3, bool> NextTarget
@@ -26,6 +41,11 @@ public class CannonController : MonoBehaviour
             return new Tuple<Vector3, bool>(position, hasTarget);
         }
     }
+    private Vector3 currentTargetPosition;
+
+    // The following enum types control the functionality of the cannon based on current state.
+        // The State enum type applies to the stage in cannon operation
+        // The AimingState essentially controls the rotation of the cannon
     private enum State
     {
         WaitForTarget,
@@ -34,7 +54,7 @@ public class CannonController : MonoBehaviour
         ApplyRotation,
         Shoot
     }
-    private State state;
+    private State state; // controls cannons current state
 
     private enum AimingState
     {
@@ -42,10 +62,14 @@ public class CannonController : MonoBehaviour
         FindRotation,
         Rotating,
     }
-    private AimingState aimState;
+    private AimingState aimState; // controls the aiming for the cannon
+
+    // The following 2 fields are used to determine in a button press has been made
     private float currentInput;
     private float previousInput;
-    private Vector3 currentTargetPosition;
+    
+    // force to apply to the projectile on shoot.
+    // calculated in CalculateForce
     private Vector3 calculatedForce; // this could be a property...
 
     void Start()
@@ -113,7 +137,8 @@ public class CannonController : MonoBehaviour
 
     void RotateToTarget()
     {
-        // TODO - Create a quaternion using the MathPhysicsEngine, then rotate to the desired rotation using Unity.
+        // TODO - Create a quaternion using the MathPhysicsEngine, then rotate to the desired rotation using Unity. 
+        // (May also try the SLERP technique to smoothly transition to new orientation)
         switch(aimState)
         {
             case AimingState.None:
@@ -121,10 +146,9 @@ public class CannonController : MonoBehaviour
                 break;
             case AimingState.FindRotation:
                 // Create a Quaternion from Euler Angles which represent the force
-                
-               
                 break;
             case AimingState.Rotating:
+                // SLERP
                 break;
             default:
                 break;
